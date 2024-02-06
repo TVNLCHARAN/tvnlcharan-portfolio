03e91bffff9100dcdc1e23bdb303c3a7a71d5ef3
TVNLCHARAN <133669809+TVNLCHARAN@users.noreply.github.com>
2024-02-06 22:15:00 +0530

    Add files via upload

---
+++ projects-data.js
@@ -0,0 +1,45 @@
+const projects = [
+    {
+        name: 'Amazon Clone',
+        description: 'I\'ve used HTML5, css3 and some simple javascript to build an amazon clone, the user can add products to the cart and the total bill will be displayed in the cart section of the webpage.',
+        img: 'amazon.png',
+        bd_color: 'rgb(128, 116, 28)',
+        link: '../javascript/javascript-amazon-project/amazon-home.html'
+
+    },
+    {
+        name: 'YouTube Clone',
+        description: 'I\'ve used HTML5, css3 and tried to build YouTube Home Page clone, I\' added some default videos to the home page and added some resources to learn web development.',
+        img: 'youtube.jpg',
+        bd_color: 'rgb(156,33,33)',
+        link: ''
+    },
+    {
+        name: 'Tic Tac Toe',
+        description: 'I\'ve used HTML5, css3 and some simple javascript to build an amazon clone, the user can add products to the cart and the total bill will be displayed in the cart section of the webpage.',
+        img: 'tic-tac-toe.jpg',
+        bd_color: 'green',
+        link: ''
+    },
+    {
+        name: 'Rock Paper Scissors',
+        description: 'I\'ve used HTML5, css3 and some simple javascript to build a simple rock paper scissors game, the <code>Player</code> can reset the score when needed, the progess of the game will be saved.',
+        img: 'rock-paper-scissors.png',
+        bd_color: 'blue',
+        link: ''
+    },
+    {
+        name: 'Amazon Clone',
+        description: 'I\'ve used HTML5, css3 and some simple javascript to build an amazon clone, the user can add products to the cart and the total bill will be displayed in the cart section of the webpage.',
+        img: 'amazon.png',
+        bd_color: 'rgb(128, 116, 28)',
+        link: ''
+    },
+    {
+        name: 'Amazon Clone',
+        description: 'I\'ve used HTML5, css3 and some simple javascript to build an amazon clone, the user can add products to the cart and the total bill will be displayed in the cart section of the webpage.',
+        img: 'amazon.png',
+        bd_color: 'rgb(128, 116, 28)',
+        link: ''
+    }
+]
---
+++ projects.js
@@ -0,0 +1,65 @@
+
+const projects_data = [
+    {
+        name: 'Amazon Clone',
+        description: 'I\'ve used HTML5, css3 and some simple javascript to build an amazon clone, the user can add products to the cart and the total bill will be displayed in the cart section of the webpage.',
+        img: 'amazon.png',
+        bd_color: 'rgb(128, 116, 28)',
+        link: 'C:/Users/91707/Desktop/WT/javascript/javascript-amazon-project'
+    },
+    {
+        name: 'YouTube Clone',
+        description: 'I\'ve used HTML5, css3 and tried to build YouTube Home Page clone, I\' added some default videos to the home page and added some resources to learn <code>web development.</code>',
+        img: 'youtube.jpg',
+        bd_color: 'rgb(156,33,33)',
+        link: 'C:/Users/91707/Desktop/WT/html&css/youtube/YouTube.html'
+    },
+    {
+        name: 'Fashion MNist using Deep Learning',
+        description: 'Fashion Mnist is the first dataset I\'ve worked on to build a Convolutional Neural Networks<code>(CNN)</code>, It is a great start for anyone who is looking to work in the field of computer vision',
+        img: 'fashion_mnist.webp',
+        bd_color: 'purple',
+        link: 'C:/Users/91707/Desktop/WT/javascript/rock-paper-scissors/rock_paper_scissors.html'
+    },
+    {
+        name: 'Tic Tac Toe',
+        description: 'Tic Tac Toe is a popular two player game, I built this using basic html, css and javascript, the players can reset the game once it is over.',
+        img: 'tic-tac-toe.jpg',
+        bd_color: 'green',
+        link: 'C:/Users/91707/Desktop/WT/javascript/tic-tac-toe/tic-tac-toe.html'
+    },
+    {
+        name: 'Rock Paper Scissors',
+        description: 'I\'ve used HTML5, css3 and some simple javascript to build a simple rock paper scissors game, the <code>Player</code> can reset the score when needed, the progess of the game will be saved.',
+        img: 'rock-paper-scissors.png',
+        bd_color: 'blue',
+        link: 'C:/Users/91707/Desktop/WT/javascript/rock-paper-scissors/rock_paper_scissors.html'
+    },
+    {
+        name: 'Bull Dozer Price Prediction using ML',
+        description: 'This is my beginner <code>Machine Learning</code> project that i worked on initially, I\'ve downloaded the dataset from kaggle and built my machine learning model using <code>RandomForestRegressor.</code>',
+        img: 'bull_dozer_price_prediction.png',
+        bd_color: 'teal',
+        link: 'C:/Users/91707/Desktop/WT/javascript/rock-paper-scissors/rock_paper_scissors.html'
+    },
+    
+]
+
+let projects = '';
+
+projects_data.forEach((project)=>{
+    projects += `<div class="col-lg-3 col-md-4 col-sm-6 col-xs-8">
+    <div class="card" style="width: 14rem;background-color: black;color: white;border: 1px solid ${project.bd_color};box-shadow: 1px 1px 10px ${project.bd_color};">
+        <img src="${project.img}" class="card-img-top" alt="project-img">
+        <div class="card-body">
+        <h5 class="card-title"><code>${project.name}</code></h5>
+        <p class="card-text">${project.description}</p>
+        <a href="${project.link}" target="_blank" class="btn btn-primary" style="color: white;font-weight: bold;font-size: 16px;background-color:${project.bd_color}; border: 1px solid ${project.bd_color}">Visit <code>${project.name}</code></a>
+        </div>
+    </div>
+</div>`
+});
+
+if(projects){
+    document.querySelector('.projects-div').innerHTML = projects;
+}
